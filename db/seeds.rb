e3a39da070cd2fb95d7860f2922177e951154b9f
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2020-11-20 13:51:54 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -11,4 +12,5 @@ Ingredient.create(name: "vodka")
 Ingredient.create(name: "gin")
 Ingredient.create(name: "rhum")
 Ingredient.create(name: "coca")
+
 
